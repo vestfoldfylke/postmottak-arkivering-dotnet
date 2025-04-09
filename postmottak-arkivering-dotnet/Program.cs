1d008866caba240e6e04ae91aba1f51c4f7f45ac
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2025-04-09 14:57:10 +0200

    Added skeleton for Archive functions

--- postmottak-arkivering-dotnet/Program.cs
+++ postmottak-arkivering-dotnet/Program.cs
@@ -1,9 +1,15 @@
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Hosting;
+using postmottak_arkivering_dotnet.Middleware;
+using VFK.Extensions.Logging;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
+
+builder.UseMiddleware<ErrorHandlingMiddleware>();
+
+builder.Logging.AddVfkLogging();
 
 // Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
 // builder.Services
