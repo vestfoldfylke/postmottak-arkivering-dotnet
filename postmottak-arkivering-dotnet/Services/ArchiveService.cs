9d4257e409f694da4c499d47b175f9c7782b89ff
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2025-04-25 13:33:49 +0200

    Add ArchiveCustom method and enhance ArchiveService with new functionalities

--- postmottak-arkivering-dotnet/Services/ArchiveService.cs
+++ postmottak-arkivering-dotnet/Services/ArchiveService.cs
@@ -13,13 +13,15 @@ namespace postmottak_arkivering_dotnet.Services;
 public interface IArchiveService
 {
     Task<JsonNode?> Archive(ArchivePayload payload);
+    Task<JsonNode?> ArchiveCustom(object payload, string route);
     Task<JsonNode> CreateCase(object parameter);
-    // CreateDocument
-    // SignOff
-    // SyncEnterprise
-    // SyncPrivatePerson
-    // UploadFiles
+    Task<JsonNode> CreateDocument(object parameter);
     Task<JsonArray> GetCases(object parameter);
+    Task<JsonNode> SignOff(object parameter);
+
+    Task<JsonNode> SyncEnterprise(string organizationNr);
+
+    Task<JsonNode> SyncPrivatePerson(object privatePerson);
 }
 
 public class ArchiveService : IArchiveService
@@ -29,6 +31,8 @@ public class ArchiveService : IArchiveService
 
     private readonly HttpClient _archiveClient;
     private readonly string[] _scopes;
+
+    private readonly JsonSerializerOptions _indentedSerializer = new JsonSerializerOptions { WriteIndented = true };
 
     public ArchiveService(IConfiguration config, IAuthenticationService authService, ILogger<ArchiveService> logger)
     {
@@ -54,43 +58,6 @@ public class ArchiveService : IArchiveService
             BaseAddress = new Uri(config["ARCHIVE_BASE_URL"] ?? throw new NullReferenceException("Archive base URL cannot be null"))
         };
     }
-
-    public async Task<JsonArray> GetCases(object parameter)
-    {
-        var payload = new ArchivePayload
-        {
-            method = "GetCases",
-            service = "CaseService",
-            parameter = parameter
-        };
-        
-        if (await Archive(payload) is not JsonArray result)
-        {
-            _logger.LogError("Failed to get cases with Parameter {@Parameter}", parameter);
-            throw new InvalidOperationException($"Failed to get cases with Parameter {JsonSerializer.Serialize(parameter)}");
-        }
-
-        return result;
-    }
-    
-    public async Task<JsonNode> CreateCase(object parameter)
-    {
-        var payload = new ArchivePayload
-        {
-            method = "CreateCase",
-            service = "CaseService",
-            parameter = parameter
-        };
-
-        var result = await Archive(payload);
-        if (result is null)
-        {
-            _logger.LogError("Failed to create case with Parameter {@Parameter}", parameter);
-            throw new InvalidOperationException($"Failed to create case with Parameter {JsonSerializer.Serialize(parameter)}");
-        }
-
-        return result;
-    }
     
     public async Task<JsonNode?> Archive(ArchivePayload payload)
     {
@@ -106,10 +73,137 @@ public class ArchiveService : IArchiveService
         if (!result.IsSuccessStatusCode)
         {
             var errorMessage = JsonSerializer.Deserialize<ArchiveErrorMessage>(resultContent, new JsonSerializerOptions{ PropertyNamingPolicy = JsonNamingPolicy.CamelCase}) ?? throw new InvalidOperationException("Failed to deserialize error message");
-            _logger.LogError("Archive error: {Message} : StatusCode: {StatusCode}. Data: {@Data}", errorMessage.Message, result.StatusCode, errorMessage.Data);
+            _logger.LogError("Archive error with Payload {@Payload}: {Message} : StatusCode: {StatusCode}. Data: {@Data}", payload, errorMessage.Message, result.StatusCode, errorMessage.Data);
+            throw new InvalidOperationException(JsonSerializer.Serialize(
+                new { errorMessage.Message, result.StatusCode, errorMessage.Data, Payload = payload },
+                _indentedSerializer));
+        }
+
+        return JsonNode.Parse(resultContent);
+    }
+    
+    public async Task<JsonNode?> ArchiveCustom(object payload, string route)
+    {
+        var token = await _authService.GetAccessToken(_scopes);
+        
+        _archiveClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.Token);
+        
+        var body = new StringContent(JsonSerializer.Serialize(payload), System.Text.Encoding.UTF8, "application/json");
+        
+        var result = await _archiveClient.PostAsync(route, body);
+        var resultContent = await result.Content.ReadAsStringAsync();
+        
+        if (!result.IsSuccessStatusCode)
+        {
+            var errorMessage = JsonSerializer.Deserialize<ArchiveErrorMessage>(resultContent, new JsonSerializerOptions{ PropertyNamingPolicy = JsonNamingPolicy.CamelCase}) ?? throw new InvalidOperationException("Failed to deserialize error message");
+            _logger.LogError("Archive error with Payload {@Payload}: {Message} : StatusCode: {StatusCode}. Data: {@Data}", payload, errorMessage.Message, result.StatusCode, errorMessage.Data);
             throw new InvalidOperationException(errorMessage.Message);
         }
 
         return JsonNode.Parse(resultContent);
+    }
+
+    public async Task<JsonNode> CreateCase(object parameter)
+    {
+        var payload = new ArchivePayload
+        {
+            service = "CaseService",
+            method = "CreateCase",
+            parameter = parameter
+        };
+
+        var result = await Archive(payload);
+        if (result is null)
+        {
+            _logger.LogError("Failed to create case with Parameter {@Parameter}", parameter);
+            throw new InvalidOperationException($"Failed to create case with Parameter {JsonSerializer.Serialize(parameter, _indentedSerializer)}");
+        }
+
+        return result;
+    }
+    
+    public async Task<JsonNode> CreateDocument(object parameter)
+    {
+        var payload = new ArchivePayload
+        {
+            service = "DocumentService",
+            method = "CreateDocument",
+            parameter = parameter
+        };
+
+        var result = await Archive(payload);
+        if (result is null)
+        {
+            _logger.LogError("Failed to create document with Parameter {@Parameter}", parameter);
+            throw new InvalidOperationException($"Failed to create document with Parameter {JsonSerializer.Serialize(parameter, _indentedSerializer)}");
+        }
+
+        return result;
+    }
+    
+    public async Task<JsonArray> GetCases(object parameter)
+    {
+        var payload = new ArchivePayload
+        {
+            service = "CaseService",
+            method = "GetCases",
+            parameter = parameter
+        };
+        
+        if (await Archive(payload) is not JsonArray result)
+        {
+            _logger.LogError("Failed to get cases with Parameter {@Parameter}", parameter);
+            throw new InvalidOperationException($"Failed to get cases with Parameter {JsonSerializer.Serialize(parameter, _indentedSerializer)}");
+        }
+
+        return result;
+    }
+    
+    public async Task<JsonNode> SignOff(object parameter)
+    {
+        var payload = new ArchivePayload
+        {
+            service = "DocumentService",
+            method = "SignOffDocument",
+            parameter = parameter
+        };
+
+        var result = await Archive(payload);
+        if (result is null)
+        {
+            _logger.LogError("Failed to sign off with Parameter {@Parameter}", parameter);
+            throw new InvalidOperationException($"Failed to sign off with Parameter {JsonSerializer.Serialize(parameter, _indentedSerializer)}");
+        }
+
+        return result;
+    }
+    
+    public async Task<JsonNode> SyncEnterprise(string organizationNr)
+    {
+        var payload = new
+        {
+            orgnr = organizationNr
+        };
+
+        var result = await ArchiveCustom(payload, "syncEnterprise");
+        if (result is null)
+        {
+            _logger.LogError("Failed to sync enterprise with Payload {@Payload}", payload);
+            throw new InvalidOperationException($"Failed to sync enterprise with Payload {JsonSerializer.Serialize(payload, _indentedSerializer)}");
+        }
+
+        return result;
+    }
+    
+    public async Task<JsonNode> SyncPrivatePerson(object privatePerson)
+    {
+        var result = await ArchiveCustom(privatePerson, "syncPrivatePerson");
+        if (result is null)
+        {
+            _logger.LogError("Failed to sync private person with Payload {@Payload}", privatePerson);
+            throw new InvalidOperationException($"Failed to sync private person with Payload {JsonSerializer.Serialize(privatePerson, _indentedSerializer)}");
+        }
+
+        return result;
     }
 }
