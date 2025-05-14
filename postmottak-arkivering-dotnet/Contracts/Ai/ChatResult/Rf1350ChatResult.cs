2525ca1c85a1ae5310df7404afbec254a5cc8b4d
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2025-05-14 08:43:14 +0200

    Add properties for project details and update type description in Rf1350ChatResult model

--- postmottak-arkivering-dotnet/Contracts/Ai/ChatResult/Rf1350ChatResult.cs
+++ postmottak-arkivering-dotnet/Contracts/Ai/ChatResult/Rf1350ChatResult.cs
@@ -6,14 +6,19 @@ public class Rf1350ChatResult
 {
     [Description("Er 9 siffer langt og kan inneholde mellomrom")]
     public string OrganizationNumber { get; init; } = string.Empty;
+    
     [Description("Ligger alltid etter Prosjektnavn:")]
     public string ProjectName { get; init; } = string.Empty;
+    
     [Description("Ligger alltid etter Prosjekteier:")]
     public string ProjectOwner { get; init; } = string.Empty;
+    
     [Description("Er på formatet: 00-0000")]
     public string ProjectNumber { get; init; } = string.Empty;
+    
     [Description("Er på formatet: 0000-0000")]
     public string ReferenceNumber { get; init; } = string.Empty;
-    [Description("Skal alltid være en av følgende typer og du må selv finne ut hvilken som stemmer ut fra input:\n- 'Anmodning om Sluttutbetaling'\n- 'Automatisk kvittering på innsendt søknad'\n- 'Overføring av mottatt søknad'")]
+    
+    [Description("Skal alltid være en av følgende typer og du må selv finne ut hvilken som stemmer ut fra input:\n- 'Anmodning om utbetaling'\n- 'Automatisk kvittering på innsendt søknad'\n- 'Overføring av mottatt søknad'")]
     public string Type { get; init; } = string.Empty;
 }
