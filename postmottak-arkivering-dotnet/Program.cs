d8ffc45e2799b973e5fbeaeea4fc20ade0cc994d
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2025-05-19 09:47:47 +0200

    Enable Application Insights and update .gitignore to exclude .DS_Store

--- postmottak-arkivering-dotnet/Program.cs
+++ postmottak-arkivering-dotnet/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,9 +29,8 @@ builder.Services.AddSingleton<IAiPluginTestService, AiPluginTestService>();
 
 AiHelper.ConfigurationManager = builder.Configuration;
 
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-// builder.Services
-//     .AddApplicationInsightsTelemetryWorkerService()
-//     .ConfigureFunctionsApplicationInsights();
+builder.Services
+     .AddApplicationInsightsTelemetryWorkerService()
+     .ConfigureFunctionsApplicationInsights();
 
 builder.Build().Run();
