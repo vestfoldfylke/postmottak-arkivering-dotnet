e5170a9d9ebc32469f58dae72c0d6110e11a1380
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2025-06-02 10:48:10 +0200

    Update PengetransportenEmailType to set match result as 'No' for unmatched emails

--- postmottak-arkivering-dotnet/EmailTypes/PengetransportenEmailType.cs
+++ postmottak-arkivering-dotnet/EmailTypes/PengetransportenEmailType.cs
@@ -98,7 +98,7 @@ public class PengetransportenEmailType : IEmailType
         {
             return new EmailTypeMatchResult
             {
-                Matched = EmailTypeMatched.Maybe,
+                Matched = EmailTypeMatched.No,
                 Result = "Emnet samsvarer ikke med noen av de forventede fakturarelaterte emnene"
             };
         }
