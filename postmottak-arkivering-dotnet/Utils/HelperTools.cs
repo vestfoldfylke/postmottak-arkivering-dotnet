00f460c6eba0ce2fc463e6bf58cf66e32ed3a662
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2025-06-03 11:02:03 +0200

    Refactor GenerateHtmlBox method to use interpolated string syntax for improved readability and remove padding

--- postmottak-arkivering-dotnet/Utils/HelperTools.cs
+++ postmottak-arkivering-dotnet/Utils/HelperTools.cs
@@ -4,11 +4,12 @@ namespace postmottak_arkivering_dotnet.Utils;
 
 public static class HelperTools
 {
-    public static string GenerateHtmlBox(string message) => @$"
-        <div style='border: 1pt solid #848484; padding: 10pt; background-color: #fff4d0; font-size: 12pt;
-                    line-height: 12pt; font-family: 'Arial'; color: Black; text-align: left;'>
+    public static string GenerateHtmlBox(string message) => $"""
+        <div style="border: 1pt solid #848484; background-color: #fff4d0; font-size: 12pt;
+                    line-height: 12pt; font-family: 'Arial'; color: Black; text-align: left;">
             {message}
-        </div>";
+        </div>
+        """;
     
     public static DateTimeOffset GetDateTimeOffset(DateTime? dateTime = null, string timeZone = "Europe/Oslo") =>
         TimeZoneInfo.ConvertTime(dateTime ?? DateTime.Now, TimeZoneInfo.FindSystemTimeZoneById(timeZone));
